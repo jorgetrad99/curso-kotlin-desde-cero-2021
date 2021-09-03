0b66398cc7a7398c5696eaf242414cea718884b1
Jorge Trad <jetradl@gmail.com>
2021-09-03 16:33:22 -0500

    Variables in Kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,15 @@
+const val PI = 3.1416    //Constantes. Van fuera de la función. Se dice que declaramos en tiempo de compilación
+
 fun main(args: Array<String>) {
+    //Todo lo que esté aquí, está declarado en tiempo de ejecución
     println("Hello World!")
 
-    // Try adding program arguments at Run/Debug configuration
-    println("Program arguments: ${args.joinToString()}")
+    var dinero : Int = 10   //Variable de lectura y escritura
+    println(dinero)
+
+    dinero = 5              //Asignando el valor 5 a la variable dinero
+    println(dinero)
+    val nombre : String = "Maria"    //Variable solo de lectura
+    println(nombre)
+    println(PI)
 }
