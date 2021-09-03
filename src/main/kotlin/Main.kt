1f56c3a31b8658f6f6a4bb9140fa37a94fb2b320
Jorge Trad <jetradl@gmail.com>
2021-09-03 17:16:35 -0500

    Kotlin and its variable types

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,7 +9,27 @@ fun main(args: Array<String>) {
 
     dinero = 5              //Asignando el valor 5 a la variable dinero
     println(dinero)
-    val nombre : String = "Maria"    //Variable solo de lectura
-    println(nombre)
+    val unNombre : String = "Maria"    //Variable solo de lectura
+    println(unNombre)
     println(PI)
+
+    val boolean : Boolean = true
+    //val numeroLargo : Long = 30000000
+    //Inferencia de Tipo
+    val numeroLargo = 3L    //Con esto, Kotlin ya sabe que es un Long
+    val double : Double = 2.6787687
+    //val float : Float = 1.1f
+    val float = 1.1f
+
+    val primerValor = 20
+    val segundoValor = 10
+
+    val tercerValor = primerValor - segundoValor
+    println("El primer valor $primerValor - segundo valor $segundoValor = $tercerValor")    //Interpolación o Template: variables dentro de cadenas de texto
+
+    //Cadenas de texto
+    val apellido = "Trad"
+    val nombre = "Jorge"
+    val nombreCompleto = "Mi nombre completo es $nombre $apellido"
+    println(nombreCompleto)
 }
